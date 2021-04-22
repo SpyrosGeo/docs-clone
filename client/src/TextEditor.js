8a375a8792dea3502d9af2b8ec0268a0a9a41bac
SpyrosGeo <xarodasimba@gmail.com>
2021-04-22 21:12:47 +0300

    added mongodb

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,9 +1,10 @@
-import React, { useCallback,useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import Quill from 'quill'
 import "quill/dist/quill.snow.css"
 import { io } from 'socket.io-client'
+import { useParams } from 'react-router-dom'
 
-
+const SAVE_INTERVAL = 2000;
 const TOOLBAR_OPTIONS = [
     [{ header: [1, 2, 3, 4, 5, 6, false] }],
     [{ font: [] }],
@@ -14,49 +15,75 @@ const TOOLBAR_OPTIONS = [
     [{ align: [] }],
     ["image", "blockquote", "code-block"],
     ["clean"],
-]
+];
 
 
 
 
 
 export default function TextEditor() {
-    const [socket,setSocket] = useState()
-    const [quill,setQuill] = useState()
+    const { id: documentId } = useParams()
+    const [socket, setSocket] = useState()
+    const [quill, setQuill] = useState()
 
-    useEffect(()=>{
-       const s =  io("http://localhost:3001")
-       setSocket(s)
+    useEffect(() => {
+        const s = io("http://localhost:3001");
+        setSocket(s);
 
-       return ()=>{
-           s.disconnect()
-       }
-    },[])
-
-    useEffect(()=>{
-        if(socket==null || quill==null ) return
-        const handler = (delta,oldDelta,source)=>{
-            if(source !=='user') return
-            socket.emit('send-changes',delta)
+        return () => {
+            s.disconnect();
         }
-        quill.on('text-change',handler)
+    }, []);
 
-        return ()=>{
-            quill.off('text-change',handler)
-        }
-    },[socket,quill])
 
-    useEffect(()=>{
-        if(socket==null || quill==null ) return
-        const handler = (delta)=>{
-          quill.updateContents(delta)
-        }
-        socket.on('receive-changes',handler)
+    useEffect(() => {
+        if (socket == null || quill == null) return
 
-        return ()=>{
-            socket.off('receive-changes',handler)
+        socket.once('load-document', document => {
+            quill.setContents(document)
+            quill.enable()
+        })
+
+        socket.emit('get-document', documentId)
+
+    }, [socket, quill, documentId])
+
+    useEffect(() => {
+        if (socket == null || quill == null) return
+        const interval =setInterval(()=>{
+            socket.emit('save-document',quill.getContents())
+        },SAVE_INTERVAL)
+        return () =>{
+            clearInterval(interval);
         }
-    },[socket,quill])
+    }, [socket, quill])
+
+
+
+    useEffect(() => {
+        if (socket == null || quill == null) return
+        const handler = (delta, oldDelta, source) => {
+            if (source !== 'user') return
+            socket.emit('send-changes', delta)
+        }
+        quill.on('text-change', handler)
+
+        return () => {
+            quill.off('text-change', handler)
+        }
+    }, [socket, quill])
+
+    useEffect(() => {
+        if (socket == null || quill == null) return
+        const handler = (delta) => {
+            quill.updateContents(delta)
+        }
+        socket.on('receive-changes', handler)
+
+        return () => {
+            socket.off('receive-changes', handler)
+        }
+    }, [socket, quill])
 
 
     // const wrapperRef = useRef()
@@ -70,6 +97,8 @@ export default function TextEditor() {
                 toolbar: TOOLBAR_OPTIONS,
             }
         })
+        q.enable(false)
+        q.setText('Loading...')
         setQuill(q)
     }, [])
     return (
