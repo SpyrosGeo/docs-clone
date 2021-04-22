aac1752f899a83e3ddb4883c968f5ab3d587d01c
SpyrosGeo <xarodasimba@gmail.com>
2021-04-22 21:16:45 +0300

    added user and password of db to .env

--- server/server.js
+++ server/server.js
@@ -1,13 +1,54 @@
+const mongoose = require('mongoose')
+const Document = require('./schemas/Document')
+require('dotenv').config()
+
+const user = process.env.DB_USER
+const password = process.env.DB_PASSWORD
+
+const mongoURI = `mongodb+srv://${user}:${password}@googledocsdemo.djyxp.mongodb.net/docs?retryWrites=true&w=majority`
+
+const options = {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}
+try {
+    mongoose.connect(mongoURI,options,()=>{
+        console.log('connected to db')
+    })
+}catch(error){
+    console.log(error)
+
+}
+
+
 const io = require('socket.io')(3001,{
     cors:{
         origin:'http://localhost:3000',
         methods:['GET','POST']
     }
 })
-
+const defaultValue = ""
 
 io.on('connection',socket =>{
-    socket.on('send-changes',delta =>{
-        socket.broadcast.emit('receive-changes',delta)
+    socket.on('get-document',async documentId=>{
+        const document = await findOrCreateDocument(documentId)
+        socket.join(documentId)
+        socket.emit('load-document',document.data)
+
+        socket.on('send-changes',delta =>{
+            socket.broadcast.to(documentId).emit('receive-changes',delta)
+        })
+
+        socket.on("save-document",async data =>{
+            await Document.findByIdAndUpdate(documentId,{data})
+        })
     })
 })
+
+
+async function findOrCreateDocument(id){
+    if(id ==null)return 
+    const document = await Document.findById(id);
+    if(document) return document;
+    return await Document.create({_id:id,data:defaultValue})
+}
---
+++ server/test.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose')
+
+
+mongoose.connect(`mongodb+srv://thatguy:124888@googledocsdemo.djyxp.mongodb.net/docs?retryWrites=true&w=majority`).then(console.log('connected'))
